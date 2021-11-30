418217577c102276441a34c068745195417d358d
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-11-30 02:49:31 -0500

    display footer and main nav

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./normalize.css";
 import "./App.scss";
 
+import MainNav from "./components/MainNav";
 import Home from "./pages/Home";
 import BookDetail from "./pages/BookDetail";
+import Footer from "./components/Footer";
 
 
 export default function App() {
   return (
     <Router>
+      <MainNav />
       <main>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/book/:bookId" element={<BookDetail />} />
         </Routes>
       </main>
+      <Footer />
     </Router>
   );
 }
