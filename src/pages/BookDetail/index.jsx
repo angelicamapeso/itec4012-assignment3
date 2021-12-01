d3b7f1e3a64a04051186768b1ab4a0ea807f57ae
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-11-30 20:50:18 -0500

    add bookdetail structure

--- src/pages/BookDetail/index.jsx
+++ src/pages/BookDetail/index.jsx
@@ -2,7 +2,9 @@ import "./styles.scss";
 
 import { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { MdCalendarToday } from "react-icons/md";
 
+import { MONTHS, BLANK_IMG } from "../../constants";
 import { processBookApiData } from "../../formatters";
 import BookContext from "../../context/BookContext";
 
@@ -36,7 +38,35 @@ export default function BookDetail() {
     }
   }
 
+  const getPublishDate = (dateString) => {
+    const date = new Date(dateString);
+    return `${MONTHS[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+  }
+
   return (
-    <h2>Book Detail: {book ? book.title : ''}</h2>
+    <section className={`book-detail ${ loading ? 'loading' : ''}`} style={{ backgroundColor: book && book.color ? book.color : 'white' }}>
+      <div className="content-container">
+        <div className="img-container">
+          <img className="img" src={book ? book.img : BLANK_IMG} alt="" />
+        </div>
+        <div className="info">
+          <div className="text">
+            <p className="main-genre">{ book ? book.genres[0] : <br /> }</p>
+            <h2 className="title">{ book ? book.title : <br /> }</h2>
+            <p className="author">{ book ? book.author : <br /> }</p>
+            <p className="date">
+              { book ? <MdCalendarToday className="icon" /> : null }
+              { book ? 'Published ' + getPublishDate(book.publish_date) : <br /> }
+            </p>
+          </div>
+          <div className="genres">
+            { book ?
+              book.genres.map((genre, i) => <p key={i} className="genre-tag">{ genre }</p>) :
+              null
+            }
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
